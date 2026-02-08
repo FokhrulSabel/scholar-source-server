7dd9adde9078719fd350c578af92129d5f47ec39
FokhrulSabel <fokhrulislam316@gmail.com>
2026-02-08 20:26:16 +0600

    Admin: Add Scholarship API

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ async function run() {
 
     const db = client.db("ScholarSourceDB");
     const userCollection = db.collection("users");
+    const scholarCollection = db.collection("scholarships");
 
     // User related API
     app.post("/users", async (req, res) => {
@@ -68,6 +69,116 @@ async function run() {
       } catch (error) {
         console.error(error);
         res.status(500).send({ message: "Server Error" });
+      }
+    });
+
+    // scholarships releted api
+
+    // Admin: Add Scholarship API
+    app.post("/scholarships", async (req, res) => {
+      try {
+        const scholarship = req.body;
+
+        // basic validation
+        if (!scholarship.scholarshipName || !scholarship.universityName) {
+          return res.status(400).send({
+            message: "Required fields missing",
+          });
+        }
+
+        const newScholarship = {
+          ...scholarship,
+          applicationDeadline: new Date(scholarship.applicationDeadline),
+          scholarshipPostDate: new Date(),
+          postedUserEmail: req.decoded.email,
+        };
+
+        const result = await scholarCollection.insertOne(newScholarship);
+
+        res.status(201).send({
+          success: true,
+          insertedId: result.insertedId,
+        });
+      } catch (error) {
+        res.status(500).send({ message: error.message });
+      }
+    });
+
+    app.get("/all-scholarships", async (req, res) => {
+      try {
+        let {
+          limit = 8,
+          sort,
+          search = "",
+          subject = "",
+          country = "",
+          degree = "",
+          page = 1,
+        } = req.query;
+        limit = parseInt(limit);
+        page = parseInt(page);
+        const query = {};
+        if (subject) query.subjectCategory = subject;
+        if (country) query.universityCountry = country;
+        if (degree) query.degree = degree;
+        if (search.trim()) {
+          query.$text = { $search: search };
+        }
+        const total = await scholarCollection.countDocuments(query);
+        let cursor = scholarCollection.find(query);
+        if (sort === "top") cursor = cursor.sort({ applicationFees: 1 });
+        const skip = (page - 1) * limit;
+        cursor = cursor.skip(skip).limit(limit);
+        const scholarships = await cursor.toArray();
+        res.json({
+          total,
+          page,
+          limit,
+          totalPages: Math.ceil(total / limit),
+          scholarships,
+        });
+      } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+      }
+    });
+
+    app.get("/scholarships", async (req, res) => {
+      try {
+        const { limit = 6, page = 1, sort, search, email } = req.query;
+        let query = {};
+
+        if (email) {
+          query.email = email;
+        }
+
+        if (search) {
+          query.scholarshipName = { $regex: search, $options: "i" };
+        }
+
+        const skip = (parseInt(page) - 1) * parseInt(limit);
+
+        let cursor = scholarCollection.find(query);
+
+        if (sort === "top") {
+          cursor = cursor.sort({ rating: -1 });
+        }
+
+        const scholarships = await cursor
+          .skip(skip)
+          .limit(parseInt(limit))
+          .toArray();
+
+        const totalCount = await scholarCollection.countDocuments(query);
+
+        res.status(200).json({
+          scholarships,
+          totalCount,
+        });
+      } catch (error) {
+        res.status(500).send({
+          success: false,
+          message: error.message,
+        });
       }
     });
 
